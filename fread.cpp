e61fb0fa9d0424d2eeb4c973ab6cd57cb1fca878
JayGao <gaojy16@mails.tsinghua.edu.cn>
2017-12-02 15:10:56 +0800

    完美

--- fread.cpp
+++ fread.cpp
@@ -1,10 +1,12 @@
+#include <cstdio>
+#include <cctype>
 const int len=456733;
 char buf[len],*cur(buf),*next(buf+1);
 #define readBuf(){\
 if(++cur==next)\
 next=(cur=buf)+fread(buf,1,sizeof(buf),stdin);\
 }
-#define getInt(x){\
+#define getInt(){\
 int s(0);\
 while(!isdigit(*cur))\
 readBuf();\
